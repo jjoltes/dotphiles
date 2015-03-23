5fdacdf34c4ebd6cd55e4742a66a91f09d45f0f9
Jeff Joltes <jeff.joltes@gmail.com>
2015-03-23 12:59:02 -0600

    new convenience aliases for general use and work rdp

--- zsh/config/aliases.zsh
+++ zsh/config/aliases.zsh
@@ -7,8 +7,25 @@ alias g8='nocorrect g8'
 # -------------------------------------------------------------------
 # search
 # -------------------------------------------------------------------
-alias grep='grep --color=auto --exclude-dir=".svn"'
-alias ff='find . -name \!*:q -ls'
+alias grep='grep --color=auto --exclude-dir=".svn,.git"'
+alias sgrep='grep -R -n -H -C 5 --exclude-dir={.git,.svn,CVS}'
+
+alias duf='du -sh *'
+alias fd='find . -type d -name'
+alias ff='find . -type f -name'
+
+##globals
+# Command line head / tail shortcuts
+alias -g H='| head'
+alias -g T='| tail'
+alias -g G='| grep'
+alias -g L="| less"
+alias -g M="| most"
+alias -g LL="2>&1 | less"
+alias -g CA="2>&1 | cat -A"
+alias -g NE="2> /dev/null"
+alias -g NUL="> /dev/null 2>&1"
+alias -g P="2>&1| pygmentize -l pytb"
 
 # -------------------------------------------------------------------
 # directory movement
@@ -18,9 +35,6 @@ alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 alias 'bk=cd $OLDPWD'
-
-alias dotfiles="cd /local/jjoltes/centos/code/.dotfiles"
-alias pushdotfiles="pushd /local/jjoltes/centos/code/.dotfiles"
 
 alias testsw="cd /projects/orion-vpu-exploration-missions_d1334/software/test_sw"
 alias pushtestsw="pushd /projects/orion-vpu-exploration-missions_d1334/software/test_sw"
@@ -39,13 +53,17 @@ alias 'dus=du -sckx * | sort -nr' #directories sorted by size
 alias 'wordy=wc -w * | sort | tail -n10' # sort files in current directory by the number of words they contain
 alias 'filecount=find . -type f | wc -l' # number of files (not directories)
 
+alias rable='ls -AFtrd *(R)' nrable='ls -AFtrd *(^R)'
+
 
 # -------------------------------------------------------------------
 # remote machines
 # -------------------------------------------------------------------
-alias 'win=xfreerdp --plugin cliprdr --no-nla -D -g 1680x1026 -d sei -u jjoltes eng-termserver.ad.seakr.com'
+alias 'win_termserver=xfreerdp --plugin cliprdr --no-nla -D -g 1680x1026 -d sei -u jjoltes eng-termserver.ad.seakr.com'
+alias 'win_termserver_full=xfreerdp --plugin cliprdr --no-nla -D -g 3360x1026 -d sei -u jjoltes eng-termserver.ad.seakr.com'
 
-alias 'winfull=xfreerdp --plugin cliprdr --no-nla -D -g 3360x1026 -d sei -u jjoltes eng-termserver.ad.seakr.com'
+alias 'win_labbox=xfreerdp --plugin cliprdr --no-nla -D -g 1680x1026 -d sei -u Orion-VPU labpc-1-87'
+alias 'win_labbox_full=xfreerdp --plugin cliprdr --no-nla -D -g 3360x1026 -d sei -u Orion-VPU labpc-1-87'
 
 # -------------------------------------------------------------------
 # Git setup
@@ -70,10 +88,9 @@ alias 'ttop=top -acu $USER -s -n30' # fancy top
 # Force tmux to use 256 colors
 alias tmux='TERM=screen-256color-bce tmux'
 
-# alias to cat this file to display
-alias acat='< ~/.zsh/aliases.zsh'
-alias fcat='< ~/.zsh/functions.zsh'
-alias sz='source ~/.zshrc'
+alias h='history'
+alias hgrep="fc -El 0 | grep"
 
+alias sz='source ~/.zshrc'
 alias reload=". ~/.zshrc && echo 'ZSH config reloaded from ~/.zshrc'"
 alias zshrc="gedit ~/.zshrc && reload"
